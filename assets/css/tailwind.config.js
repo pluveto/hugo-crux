420226fb1aa3c6ff35ecaab4a7fa7436d271923a
pluveto <receding@protonmail.com>
2021-08-23 22:56:53 +0800

    fix: tailwind typography

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Josefin Sans', 'ui-sans-serif', 'system-ui'],
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            pre: { color: null, backgroundColor: '#f7f7f7' },
+           'pre code': { color: null, backgroundColor: null },
+           a: { textDecoration: 'none' }
+          }
+        }
       }
     }
   },
