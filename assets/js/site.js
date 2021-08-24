785c527e413478e9cfd3d3b1344b3efa348ea7b3
pluveto <receding@protonmail.com>
2021-08-24 18:31:45 +0800

    feat: toggleExpand shortcode

---
+++ assets/js/site.js
@@ -0,0 +1,8 @@
+function toggleExpand(who) {
+    var next = who.nextElementSibling
+    console.log(next.style.display)
+    next.style.display = next.style.display == 'block' ? 'none' : 'block'
+    next.classList.toggle('show')
+    console.log(next.style.display)
+    who.querySelector(".icon").classList.toggle('bx-rotate-90')
+}
